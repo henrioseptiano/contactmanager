7c26aa32a4943883d59a3d0e9500715f89f66375
henrio septiano <henrio.septiano@reebonz.com>
2019-04-28 11:03:31 +0700

    Updates

---
+++ precache-manifest.db33084e74911d02c7204d7e3c210edf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda5aa649625ea96cdddd5c06eaa197a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adc16ec209a5446f7621",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f0de16b5e0059f1f66a6",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "adc16ec209a5446f7621",
+    "url": "/static/js/2.1064f207.chunk.js"
+  },
+  {
+    "revision": "f0de16b5e0059f1f66a6",
+    "url": "/static/js/main.b7278f64.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
